6e3dfb3d8910a735f25c875b606a17a6162b9d09
Authentic <authenticg0199@gmail.com>
2025-04-17 21:52:36 +0600

    4. Update CoinAcceptor and update commands.txt

--- src/model/CoinAcceptor.java
+++ src/model/CoinAcceptor.java
@@ -1,17 +1,26 @@
 package model;
 
-public class CoinAcceptor {
+import util.MoneyAcceptor;
+
+public class CoinAcceptor implements MoneyAcceptor {
     private int amount;
 
-    public CoinAcceptor(int amount) {
-        this.amount = amount;
-    }
-
+    @Override
     public int getAmount() {
         return amount;
     }
 
+    @Override
     public void setAmount(int amount) {
         this.amount = amount;
     }
+
+    @Override
+    public boolean acceptBalance() {
+        amount += 25;
+        System.out.println("Текущий баланс" + amount);
+        return true;
+    }
+
+
 }
